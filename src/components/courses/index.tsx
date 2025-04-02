cc28e427888b14d2279cc4a047743993562d625b
Ashish <kapoorashish713@gmail.com>
2025-04-02 21:00:08 +0530

    courses page add

--- src/components/courses/index.tsx
+++ src/components/courses/index.tsx
@@ -1,9 +1,20 @@
-import React from 'react'
-
-const  Courses = () => {
+"use client";
+import React from "react";
+import { motion } from "motion/react";
+const Courses = () => {
   return (
-    <div>Courses</div>
-  )
-}
+    <main className="min-w-screen min-h-screen justify-center flex">
+      <motion.h1
+        initial={{ y: 100, opacity: 0 }}
+        animate={{ y: 0, opacity: 100 }}
+        transition={{ duration: 0.8 }}
+        className="text-5xl font-bold"
+      >
+        Courses
+        
+      </motion.h1>
+    </main>
+  );
+};
 
-export default Courses ;
+export default Courses;
