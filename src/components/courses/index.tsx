b0cf97fd329fe7ef4329cfa084b4bd62b27e1452
Ashish <kapoorashish713@gmail.com>
2025-04-03 07:40:18 +0530

    courses

--- src/components/courses/index.tsx
+++ src/components/courses/index.tsx
@@ -3,12 +3,12 @@ import React from "react";
 import { motion } from "motion/react";
 const Courses = () => {
   return (
-    <main className="min-w-screen min-h-screen mt-5 items-center flex flex-col">
+    <main className="min-w-screen min-h-screen mt-14 items-center flex flex-col">
       <motion.h1
         initial={{ y: 100, opacity: 0 }}
         animate={{ y: 0, opacity: 100 }}
         transition={{ duration: 0.8 }}
-        className="text-5xl font-bold"
+        className="md:text-5xl text-3xl font-bold"
       >
         Featured Courses
         
@@ -17,7 +17,7 @@ const Courses = () => {
         initial={{ y: 100, opacity: 0 }}
         animate={{ y: 0, opacity: 100 }}
         transition={{ duration: 1 }}
-        className="text-neutral-600"
+        className="text-neutral-600 w-full text-center"
       >
         Explore put most courses and start your learning journey today
         
